2edec15ddecea41e6d00445e948acfdc4472fc6a
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2022-09-25 16:21:34 +0530

    news cards are added

--- components/news/LatestNews.js
+++ components/news/LatestNews.js
@@ -1,6 +1,46 @@
 import React, { useState, useCallback, useEffect } from "react";
-import { getCryptoCompareUrl } from "@/utils/constants";
 import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
+import styled from "styled-components";
+import moment from "moment";
+import { getCryptoCompareUrl } from "@/utils/constants";
+import { Flex, Box, Text } from "@/components/atoms";
+import { getTime } from "@/utils/time";
+
+export const Cards = styled(Flex)`
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+export const Card = styled(Box)`
+  width: 275px;
+  margin: 1rem;
+  padding: 1rem;
+  background: #fff;
+  border-radius: 5px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+
+  a {
+    color: #4c4c4c;
+  }
+`;
+
+export const TextBody = styled(Text)`
+  -webkit-box-orient: vertical;
+  display: -webkit-box;
+  -webkit-line-clamp: 7;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: normal;
+  /* padding: 1rem 0; */
+  min-height: 200px;
+`;
 
 export default function LatestNews() {
   const [newsData, setNewsData] = useState([]);
@@ -8,7 +48,7 @@ export default function LatestNews() {
     const url = getCryptoCompareUrl("data/v2/news/", {
       lang: "EN",
     });
-    console.log('url', url);
+    console.log("url", url);
     axios.get(url).then((res) => {
       console.log("getPrice ", res.data);
       setNewsData(res.data.Data);
@@ -19,7 +59,38 @@ export default function LatestNews() {
     getLatestNews();
   }, [getLatestNews]);
 
-  console.log('newsData', newsData);
+  console.log("newsData", newsData);
 
-  return <div>LatestNews</div>;
+  return (
+    <Cards>
+      {newsData.map((item) => (
+        <Card key={item?.id}>
+          <Link href={item?.url} passHref>
+            <a target="_blank" rel="noopener noreferrer">
+              <Flex>
+                <Image
+                  src={item?.imageurl}
+                  alt={item?.source}
+                  width={128}
+                  height={128}
+                  style={{ objectFit: "contain" }}
+                />
+                <Text as="h4" pl="1rem">
+                  {item?.title}
+                </Text>
+              </Flex>
+              <TextBody>{item?.body}</TextBody>
+              <Text my="0.5rem">{item?.body}</Text>
+              <Text fontSize="12px" mt="1rem">
+                {getTime(item?.published_on)}
+              </Text>
+              <Text fontSize="8px">
+                {moment(getTime(item?.published_on)).fromNow()}
+              </Text>
+            </a>
+          </Link>
+        </Card>
+      ))}
+    </Cards>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["lh3.googleusercontent.com", "res.cloudinary.com", "ethplorer.io"],
+    domains: ["lh3.googleusercontent.com", "res.cloudinary.com", "ethplorer.io", "images.cryptocompare.com"],
   },
 };
 
